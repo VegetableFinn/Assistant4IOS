06a565ee17696ea1dad4af9584f1bf6ceeca71e9
VegetableFinn <hefan121@qq.com>
2016-04-22 16:39:45 +0800

    Initial Commit

---
+++ assistant4IOSTests/assistant4IOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  assistant4IOSTests.swift
+//  assistant4IOSTests
+//
+//  Created by hefan on 16/4/22.
+//  Copyright © 2016年 hefan. All rights reserved.
+//
+
+import XCTest
+@testable import assistant4IOS
+
+class assistant4IOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
